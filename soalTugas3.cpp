63ebc5ff95751ae9ae81514c25f6b9f67c6df7b5
kurniahafizz11-commits <kurniahafizz11@gmail.com>
2026-03-17 09:33:48 +0700

    Update soalTugas3.cpp

--- soalTugas3.cpp
+++ soalTugas3.cpp
@@ -1,23 +1,28 @@
-/*
-Lembaga Kemahasiswaan USU memiliki sebuah program yang menentukan kategori beasiswa yang dapat diterima oleh mahasiswa yang mengajukan beasiswa. Penentuan beasiswa ini didasarkan pada Indeks Prestasi (IP) mahasiswa dan Gaji Orang Tua.
+#include <iostream>
+using namespace std;
 
-Ketentuan Penerimaan Beasiswa
-1. Mahasiswa dengan IP ≥ 3.5 otomatis berhak atas beasiswa kategori 4.
-2.  Mahasiswa berhak atas beasiswa kategori 1 jika:
-- Gaji orang tua kurang dari 1 juta rupiah.
-- IP mahasiswa kurang dari 3.5.
-3. Mahasiswa berhak atas beasiswa kategori 2 atau 3 jika:
-- Gaji orang tua berada di rentang 1 juta - 5 juta rupiah.
-- IP mahasiswa kurang dari 3.5:
-    - Jika IP ≥ 2.0, berhak atas beasiswa kategori 3.
-    - Jika IP < 2.0, berhak atas beasiswa kategori 2.
-4. Mahasiswa yang tidak memenuhi kriteria di atas tidak mendapatkan beasiswa.
+main()
+{
+    float gaji, IP;
 
-Buatlah program yang membaca 2 input berupa bilangan riil (float):
-IP mahasiswa (contoh: 3.51)
-Gaji orang tua dalam juta rupiah (contoh: 1.5, yang berarti 1.5 juta rupiah)
-Program harus mencetak kategori beasiswa yang diperoleh mahasiswa sesuai dengan ketentuan di atas. Jika mahasiswa tidak berhak atas beasiswa, cetak “Tidak berhak mendapatkan beasiswa”.
+    system ("cls"); // berfungsi untuk membersihkan layar atau tampilan
+    cout<<"IP : ";
+    cin>>IP; // menampilkan output IP dan meminta user menginput IP 
+    cout<<"Gaji orang tua: ";
+    cin>>gaji; // menampilkan output Gaji orang tua dan meminta user menginput gaji
 
-*/
-
-// Hapus komentar yang berisi soal sebelum memasukkan kode untuk jawaban
+    // menggunakan kondisi if else if dan else untuk mengecek apakah mahasiswa tersebut termasuk beasiswa dengan kategori 1,2,3,4 atau tidak berhak
+    if (IP >=3.5) 
+        cout<<"Berhak atas beasiswa kategori 4"; // kondisi untuk mengecek jika ip >= 3.5 maka akan menampilkan berhak atas beasiswa kategori 4
+    else if (gaji < 1 && IP < 3.5 )
+        cout<<"Berhak atas beasiswa kategori 1"; // jika tidak kondisi pertama, ia akan mengecek apakah gaji < 1 dan IP < 3.5 maka akan menampilkan berhak atas beasiswa kategori 1
+    else if (gaji >=1 && gaji<=5) { 
+        if (IP >= 2)
+            cout<<"Berhak atas beasiswa kategori 3"; // jika tidak kondisi pertama atau kedua, ia akan mengecek apakah IP >= 2 dengan rentang gaji >=1 dan gaji <=5 kemudian akan menampilkan berhak atas beasiswa kategori 3
+        else
+            cout<<"Berhak atas beasiswa kategori 2"; // jika kondisi IP tidak >= 2 alias <2 dengan rentang gaji yang sama, maka akan menampilkan berhak atas beasiswa kategori 2       
+    }
+    else
+        cout<<"Tidak berhak atas beasiswa"; // jika semua kondisi tidak memenuhi maka akan tampil tidak berhak atas beasiswa
+        return 0;
+}
